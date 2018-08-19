1e23675eacd25609443bfdd2d0a35aa301a52277
fmc <franklin@peeriq.com>
2018-08-19 17:35:34 -0400

    Json implicits (#8)
    * added implicit conversions for commonly used model => json
    
    * added comment
    
    * added println

---
+++ scala/implicits.scala
@@ -0,0 +1,77 @@
+import play.api.libs.json.{Json, OWrites}
+
+import scala.util.Try
+
+
+// Generic Model
+trait GenericModel {
+  val id: String
+
+  implicit val jsWriter = OWrites[ConcreteModel] { m =>
+    Json.obj(
+      "id" -> m.id
+    )
+  }
+}
+
+
+case class ConcreteModel(description: String) extends GenericModel {
+
+  val id: String = hashCode().toString
+
+  val name: String = getClass.getSimpleName
+
+  // implicitly convert a "model" to json
+  // omitting this will revert to the jsWriter on the GenericModel
+  override implicit val jsWriter = OWrites[ConcreteModel] { m =>
+    Json.obj(
+      "id" -> m.id,
+      "name" -> m.name,
+      "description" -> m.description
+    )
+  }
+
+}
+
+
+trait DAO[M <: GenericModel] {
+
+  def create(m: M)(implicit w: OWrites[M]): Boolean = {
+    val js = Json.toJson(m)
+
+    println(Json.prettyPrint(js))
+
+    // Validate is json
+    Try(js).isSuccess
+
+  }
+
+}
+
+
+case object Repo extends DAO[ConcreteModel]
+
+
+object CustomJson {
+  implicit val jsWriterOmitName = OWrites[ConcreteModel] { m =>
+    Json.obj(
+      "id" -> m.id,
+      "description" -> m.name
+    )
+  }
+}
+
+
+
+val model = ConcreteModel("mock object")
+
+val model2Json = model.jsWriter
+
+Repo.create(model)(model2Json)  // returns true
+
+
+val omitNameJson = CustomJson.jsWriterOmitName
+
+Repo.create(model)(omitNameJson)  // returns true; omits the "name" field
+
+
