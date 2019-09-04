3fc66865c0a1f2a307fc45c64cdf13419669316b
fmc <franklin@peeriq.com>
2019-09-04 13:58:38 -0400

    [chore] Clean up line breaks in restart script

--- kafka/restart-kafka.sh
+++ kafka/restart-kafka.sh
@@ -2,4 +2,9 @@
 
 # hosts is a text file containing the ip addresses of all the kafka nodes in the cluster
 # replace PeerTestKafka.pem with the appropriate key
-parallel-ssh -i -h hosts -x "-i ~/.ssh/PeerTestKafka.pem" "sudo service kafka stop; sudo /usr/local/kafka/bin/zookeeper-server-stop.sh; sleep 20; sudo /usr/local/kafka/bin/zookeeper-server-start.sh -daemon /etc/kafka/config/zookeeper.properties; sudo service kafka start"
+parallel-ssh -i -h hosts -x "-i ~/.ssh/PeerTestKafka.pem" \
+  "sudo service kafka stop; \
+  sudo /usr/local/kafka/bin/zookeeper-server-stop.sh; \
+  sleep 20; \
+  sudo /usr/local/kafka/bin/zookeeper-server-start.sh -daemon /etc/kafka/config/zookeeper.properties; \
+  sudo service kafka start"
