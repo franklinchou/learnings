478c19a25fe72aadbdb18536dff390b78ef3cd72
fmc <franklin@peeriq.com>
2019-09-05 10:10:35 -0400

    Update delete-logs-and-restart.sh

--- kafka/delete-logs-and-restart.sh
+++ kafka/delete-logs-and-restart.sh
@@ -4,7 +4,7 @@
 parallel-ssh -i -h hosts -x "-i ~/.ssh/PeerTestKafka.pem" \
   "sudo /usr/local/kafka/bin/zookeeper-server-stop.sh; \
   sudo service kafka stop; \
-  for f in /var/kafka/*; do sudo rm -rf \"$f\"; done; \
+  for f in /var/kafka/*; do sudo rm -rf $f; done; \
   sleep 20; \
   sudo /usr/local/kafka/bin/zookeeper-server-start.sh -daemon /etc/kafka/config/zookeeper.properties; \
   sudo service kafka start"
