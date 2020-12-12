e5718991e1f26a83621a2eea285fea9668d5c432
fmc <franklin@peeriq.com>
2020-12-11 22:09:17 -0500

    Download all jpegs in a sequence (#142)
    * download script
    
    * add shebang
    
    * add usage comment
    
    * add to readme

---
+++ linux/download.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Download all the files with JPG extension in a sequence by providin the following positional parameters:
+# 1 source
+# 2 ending index
+# 3 output destination
+
+# set up (create output destination if it doesn't exist)
+if [ -d "$3" ]; then
+    echo 'choose new directory'
+    exit 1
+else
+    mkdir $3
+fi
+
+for i in $(seq -w 1 $2); do
+    source=$1
+    dest=$(echo $source | awk -v i=$i '{ gsub("0[0-9]{3}.JPG", i ".JPG"); print }')
+    $(curl -X GET "$dest" -o "$3/$i.jpg")
+done
+
